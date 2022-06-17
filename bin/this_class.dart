c3c164a17204bdac93a87c88f61b9c0e3e517972
AirSupan <airsupansa23@gmail.com>
2022-06-17 14:53:24 +0700

    first commit

--- bin/this_class.dart
+++ bin/this_class.dart
@@ -1,15 +1,14 @@
 void main(){
-  Mobile mob1 = Mobile("Samsung", 2020);
+  Mobile mob1 = Mobile("Samsung",2022);
 }
 
 class Mobile{
-  String? modelName;
+  String? modelName = "Samsung" ;
   int? manYear;
   //Create Constructor
   Mobile(String moName, int mYear){
     this.modelName = moName;
-    this.modelName; = mYear;
-    print ('$modelName,$manYear');
-
+    this.manYear = mYear;
+    print('$modelName, $manYear');
   }
 }
